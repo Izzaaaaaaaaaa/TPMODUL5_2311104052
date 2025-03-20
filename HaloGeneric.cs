539e3d55e3ae7cd8326faf1141d8664e61f1e82e
izzaaaaaaaaaa <izzatyzahara2424@gmail.com>
2025-03-20 11:17:01 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

---
+++ HaloGeneric.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPModul5_2311104052
+{
+    class HaloGeneric
+    {
+    }
+}
